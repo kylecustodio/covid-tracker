da105a66edf8594d53dad0f38f87bad8aa8806c0
Kyle Custodio <kkzyc@yahoo.com>
2020-08-17 18:48:01 -0500

    Updates

---
+++ precache-manifest.49baee33b9680f7b88f5f57c2bf40a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d19b03f86a006cc699ad7c040209f1a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "d2a301310a402073d316",
+    "url": "/covid-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ef95e8df3c50e70a824",
+    "url": "/covid-tracker/static/js/2.110d248b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-tracker/static/js/2.110d248b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a301310a402073d316",
+    "url": "/covid-tracker/static/js/main.68a2e87c.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
