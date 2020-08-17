b17a467630af3443f72c5026b85aac1cbea42061
Kyle Custodio <kkzyc@yahoo.com>
2020-08-16 22:58:19 -0500

    display all states and filter by search

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
-import { Grid, Container, Typography, Box } from '@material-ui/core';
+import { Grid, Container, Typography, Box, TextField } from '@material-ui/core';
 import NationalCards from './components/NationalCards';
+import StateCard from './components/StateCard';
 
 function App() {
   const [nationalStats, setNationalStats] = useState([]);
+  const [stateStats, setStateStats] = useState([]);
+  const [stateInfo, setStateInfo] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     getNationalStats();
+    getStateStats();
+    getStateInfo();
   }, []);
 
   const getNationalStats = async () => {
     const response = await fetch(`https://api.covidtracking.com/v1/us/current.json`);
     const data = await response.json();
     setNationalStats(data[0]);
+  }
+
+  const getStateStats = async () => {
+    const response = await fetch(`https://api.covidtracking.com/v1/states/current.json`)
+    const data = await response.json();
+    setStateStats(data);
+  }
+
+  const getStateInfo = async () => {
+    const response = await fetch(`https://api.covidtracking.com/v1/states/info.json`)
+    const data = await response.json();
+    setStateInfo(data);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
   }
 
   return (
@@ -26,6 +48,21 @@ function App() {
           </Box>
         </Typography>
         <NationalCards data={nationalStats} />
+        <Typography variant='h2'>
+          <Box textAlign='center' m={1}>
+            States and Territories
+          </Box>
+        </Typography>
+        <Box textAlign='center' my={4}>
+          <TextField label='Search' variant='outlined' fullWidth={true} value={search} onChange={updateSearch}/>
+        </Box>
+        <Grid container justify='center' spacing={3}>
+          {stateInfo.filter(state => state.name.toLowerCase().includes(search.toLowerCase())).map((filteredState, index) => (
+            <Grid item xs={6} md={3}>
+              <StateCard title={filteredState.name} cases={stateStats[index].positive} />
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </div>
   );
